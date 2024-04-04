73c0625b6d2278e024b24bb2214dd1757e870038
Alvaro herbert Medrano perez <aljak1815@gmail.com>
2024-04-04 17:11:50 -0400

    arreglo de webVersionCache

--- api.js
+++ api.js
@@ -11,6 +11,7 @@ async function startAPI() {
   const client = new Client({
     session: sessionData,
     authStrategy: new LocalAuth(),
+    webVersionCache: { type: 'remote', remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'},
   });
 
   client.on("qr", (qr) => {
